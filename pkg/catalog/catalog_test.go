a1586d8ac38038275f6d40b1f9d3c4b480a4d881
Tom Moulard <tom.moulard@traefik.io>
2023-01-26 10:59:00 +0100

    wip

--- pkg/catalog/catalog_test.go
+++ pkg/catalog/catalog_test.go
@@ -4,14 +4,25 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubemock "k8s.io/client-go/kubernetes/fake"
 )
 
-func TestWatcher_IsAvailable(t *testing.T) {
-	kubeObjects := []runtime.Object{
-		// apiextensions.CustomResourceDefinition{},
-	}
+func TestWatcher_CanWatch(t *testing.T) {
+	kubeObjects := []runtime.Object{}
 	kubeClientSet := kubemock.NewSimpleClientset(kubeObjects...)
 	assert.False(t, IsAvailable(kubeClientSet))
+
+	kubeObjects = []runtime.Object{
+		apiextensions.CustomResourceDefinition{
+			Spec: apiextensions.CustomResourceDefinitionSpec{
+				Names: apiextensions.CustomResourceDefinitionNames{
+					Kind: "Calalog",
+				},
+			},
+		},
+	}
+	kubeClientSet = kubemock.NewSimpleClientset(kubeObjects...)
+	assert.True(t, IsAvailable(kubeClientSet))
 }
